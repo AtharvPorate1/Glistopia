e0da8a30b0f1940cefab4d4923cf2399cf266096
[AtharvPorate1] <www.atharvporate254@gmail.com>
2023-02-12 00:26:32 +0530

    Added basic frontend using React...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-page">
+      <div className="header">
+        <img src="logo-placeholder.png" alt="Logo Placeholder" />
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="main-section">
+        <div className="menu">
+          <h2>Main Menu</h2>
+          <ul>
+            <li>Placeholder 1</li>
+            <li>Placeholder 2</li>
+            <li>Placeholder 3</li>
+          </ul>
+        </div>
+        <div className="center-section">
+          <img src="image-placeholder.png" alt="Image Placeholder" />
+        </div>
+        <div className="side-section">
+          <h2>Best Nearby Hostels</h2>
+          <ul>
+            <li>Hostel Placeholder 1</li>
+            <li>Hostel Placeholder 2</li>
+            <li>Hostel Placeholder 3</li>
+          </ul>
+          <h2>Best Nearby Food Places</h2>
+          <ul>
+            <li>Food Placeholder 1</li>
+            <li>Food Placeholder 2</li>
+            <li>Food Placeholder 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
