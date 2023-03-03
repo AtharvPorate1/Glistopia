1675eefa751c0407bbd11d92a7f22f81b6d0bb4c
Jayesh <jayeshhire96@gmail.com>
2023-03-03 22:32:40 +0530

    added few extra models for service providers

--- Glistopia/settings.py
+++ Glistopia/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'Glistopia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'PASSWORD': '1234',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
@@ -128,3 +132,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = "/home/jayesh/Glistopia/Glistopia/media"
--- User/models.py
+++ User/models.py
@@ -12,6 +12,8 @@ class UserModel(models.Model):
     UserId = models.AutoField(primary_key=True)
     IsStudent = models.BooleanField()
     IsServiceProvider = models.BooleanField()
+    lastLogin = models.DateTimeField("login date time ") #every time the user logins date and time should be overwritten in the date and time section
+    
 
     class Meta:
         db_table = "user"
@@ -34,6 +36,7 @@ class Student(models.Model):
 class Services(models.Model):
     services = models.TextField()
     serviceId = models.AutoField(primary_key=True)
+    
 
 
 #models for Accomodation 
@@ -58,4 +61,36 @@ class MessSeeker(models.Model):
     
     class Meta :
         db_table = "mess Seeker"
+
+#model for storing service provider data
+class ServiceProvider(models.Model):
+    UserId = models.ForeignKey(UserModel, on_delete = models.CASCADE)
+    serviceProvided = models.TextField()
+    serviceId = models.ForeignKey(Services , on_delete=models.CASCADE)
+
+
+def user_directory_path(instance ):
+    return 'user_{}'.format(instance.UserModel.UserId)
+
+#moddel for storing Accomodation provider data
+class AccomodationProvider(models.Model):
+    UserId = models.ForeignKey(UserModel , on_delete=models.CASCADE)
+    location = models.TextField()
+    images = models.ImageField(upload_to=f'uploads/Accomodation/{user_directory_path}')
+    size = models.IntegerField("Size of in square feet")
+    noOfRooms = models.TextField() #this will contain input like 1BHK , 1RK etc.
+    Rent = models.IntegerField()
+    deposit = models.IntegerField()
+    postedOn = models.DateField(auto_now_add=True)
+
+#model for storing mess service owner data
+class MessProvider():
+    userId = models.ForeignKey(UserModel , on_delete=models.CASCADE)
+    location = models.TextField()
+    isTiffinAvailable = models.BooleanField()
+    onceADay = models.IntegerField() #mess rates for one time mess facility in a day for a month 
+    twiceADay = models.IntegerField() #mess rates for twice a day for a month
+    orderRate = models.IntegerField() #normal order rates for food
+
+
     
