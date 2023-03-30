4ec9216e72551bddde2192aa6fb91a9f4d52b443
Jayesh <jayeshhire96@gmail.com>
2023-03-30 10:22:59 +0530

    testing

--- Glistopia/urls.py
+++ Glistopia/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path , include
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
-    #path('mess/',include('Mess.urls')),
-    #path('user/',include('User.urls')),
+    path('mess/',include('Mess.urls')),
+   # path('user/',include('User.urls')),
     path('admin/', admin.site.urls),
     path('',TemplateView.as_view(template_name = 'index.html'))
 ]
